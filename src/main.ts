0fa9eb10179536a08cfeea35446b74fe165367d3
Stefano Chiodino <stefano.chiodino@epiphanysolutions.co.uk>
2016-08-11 11:35:33 +0100

    Working!

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import { HtmlDestroyer } from "./html-destroyer";
 
 var htmlDestroyer:HtmlDestroyer = new HtmlDestroyer();
+
+$("h2").each(function() {
+    var element = $(this); 
+    htmlDestroyer.destroyH(element);
+});
