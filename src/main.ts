40c04031857820c40d79b99fc4537a375817d670
Stefano Chiodino <stefano.chiodino@epiphanysolutions.co.uk>
2016-08-11 11:56:51 +0100

    Destroy all h elements

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { HtmlDestroyer } from "./html-destroyer";
 
 var htmlDestroyer:HtmlDestroyer = new HtmlDestroyer();
 
-$("h2").each(function() {
+$("h1,h2,h3,h4,h5,h6").each(function() {
     var element = $(this); 
+    var htmlDestroyer:HtmlDestroyer = new HtmlDestroyer();
+    
     var destroyElement = function() {
-        var htmlDestroyer:HtmlDestroyer = new HtmlDestroyer();
         htmlDestroyer.destroyH(element);
     };
     var intervalId = setInterval(destroyElement, 200);
